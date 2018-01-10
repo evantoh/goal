25c29794b9e8af4aa4cbf67038ee0407cac30ec2
evantoh <evanmwenda@gmail.com>
2018-01-10 10:56:50 +0300

    add and initialise the goal project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';;
 import {FormsModule} from '@angular/forms'
+import {AlertsService} from './alert-service/alerts.service'
+
 
 
 import { AppComponent } from './app.component';
@@ -9,8 +11,21 @@ import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+import{HttpClientModule} from '@angular/common/http';
+import{NgProgressModule} from '@ngx-progressbar/core';
+import{NgProgressHttpClientModule} from '@ngx-progressbar/http-client';
+import { AboutComponent } from './about/about.component';
+import{RouterModule,Routes} from '@angular/router';
+import{RoutingModule} from './routing/routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
+//defining Route
+const routes:Routes=[
+{path:"goals",component:GoalComponent},
+{path:"about",component:AboutComponent},
+{path:"",redirectTo:"/goals",pathMatch:"full"},
+{path:'**',component:NotFoundComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,14 +33,23 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     GoalDetailsComponent,
     StrikethroughDirective,
     DateCountPipe,
-    GoalFormComponent
+    GoalFormComponent,
+    AboutComponent,
+    NotFoundComponent,
+
+
 
   ],
   imports: [
     BrowserModule,
-    FormsModule
+     FormsModule,
+     RoutingModule,
+     HttpClientModule,
+     NgProgressModule.forRoot(),
+     NgProgressHttpClientModule,
+
   ],
-  providers: [],
+  providers: [AlertsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import{RouterModule,Routes} from '@angular/router'
+import {GoalDetailsComponent}from '../goal-details/goal-details.component';
+
+import{GoalComponent} from '../goal/goal.component';
+import{AboutComponent} from '../about/about.component';
+import{NotFoundComponent} from '../not-found/not-found.component';
+
+const routes:Routes=[
+  {path:"goals",component:GoalComponent},
+  {path:"about",component:AboutComponent},
+  {path:"",redirectTo:"/goals",pathMatch:"full"},
+  {path:'goals/:id',component:GoalDetailsComponent},
+  {path:'**',component:NotFoundComponent}]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: []
+})
+export class RoutingModule { }
